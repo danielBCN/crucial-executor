af25e1881c9560f5e919698954c469e58fac3aa0
Daniel Barcelona <daniel.barcelona@urv.cat>
2020-06-04 14:46:02 +0200

    Initial commit

---
+++ src/main/java/crucial/execution/aws/AWSLambdaInvoker.java
@@ -0,0 +1,68 @@
+package crucial.execution.aws;
+
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.lambda.AWSLambda;
+import com.amazonaws.services.lambda.AWSLambdaClientBuilder;
+import com.amazonaws.services.lambda.model.*;
+import com.google.gson.Gson;
+
+import java.nio.ByteBuffer;
+
+/**
+ * Invoke calls at AWS Lambda.
+ *
+ * @author Daniel
+ */
+class AWSLambdaInvoker {
+    private static AWSLambda lambdaClient;
+    private String lambdaFunctionName;
+    private Gson gson = new Gson();
+
+    AWSLambdaInvoker(Regions region, String functionName) {
+        lambdaClient = AWSLambdaClientBuilder.standard()
+                .withRegion(region)
+                .withClientConfiguration(new ClientConfiguration()
+                        .withMaxConnections(1000)
+                        .withSocketTimeout(600_000)
+                ).build();
+        lambdaFunctionName = functionName;
+    }
+
+    public void initClient(){
+        GetFunctionRequest gf = new GetFunctionRequest();
+        gf.setFunctionName(lambdaFunctionName);
+        lambdaClient.getFunction(gf);
+    }
+
+    /**
+     * Synchronous Lambda invocation. With tail logs.
+     *
+     * @param payload Input for the Lambda
+     * @return InvokeResult of the call.
+     */
+    InvokeResult invoke(byte[] payload) {
+        return invoke(payload, true);
+    }
+
+    /**
+     * Synchronous Lambda invocation.
+     *
+     * @param payload  Input for the Lambda
+     * @param tailLogs Request tail logs?
+     * @return InvokeResult of the call.
+     */
+    InvokeResult invoke(byte[] payload, boolean tailLogs) {
+        InvokeRequest req = new InvokeRequest();
+        req.setFunctionName(lambdaFunctionName);
+        req.setInvocationType(InvocationType.RequestResponse);
+        req.setPayload(ByteBuffer.wrap(gson.toJson(payload).getBytes()));
+        if (tailLogs) req.setLogType(LogType.Tail);
+        return lambdaClient.invoke(req);
+    }
+
+    void stop() {
+        lambdaClient.shutdown();
+    }
+
+}
